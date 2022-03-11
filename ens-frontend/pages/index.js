740962ff49afd4cfd7cdc8a6928f67c29a25f9b6
ambiti0n <fabulousfresh010@gmail.com>
2022-03-11 05:38:36 +0100

    the state of our application is changed based on wallet connection or disconnection by the use of useEffect

--- ens-frontend/pages/index.js
+++ ens-frontend/pages/index.js
@@ -71,4 +71,22 @@ export default function Home() {
     }
   };
 
+
+  // useEffects are used to react to changes in state of the website
+  // The array at the end of function call represents what state changes will trigger this effect
+  // In this case, whenever the value of `walletConnected` changes - this effect will be called
+  useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletConnected]);
+
 }
