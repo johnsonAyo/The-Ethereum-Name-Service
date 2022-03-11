74cc5c9461a1fb94024411f9c0d9c861aa83f0b1
ambiti0n <fabulousfresh010@gmail.com>
2022-03-11 05:33:58 +0100

    provider function needed in order to interract with the blockchain  reading transactions, reading balances, reading state, etc.implemented

--- ens-frontend/pages/index.js
+++ ens-frontend/pages/index.js
@@ -14,4 +14,32 @@ export default function Home() {
   // Save the address of the currently connected contract
   const [address, setAddress] = useState("");
 
+
+  /**
+   * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
+   *
+   * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
+   * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
+   * request signatures from the user using Signer functions.
+   */
+   const getProviderOrSigner = async () => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    // If user is not connected to the Rinkeby network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 4) {
+      window.alert("Change the network to Rinkeby");
+      throw new Error("Change network to Rinkeby");
+    }
+    const signer = web3Provider.getSigner();
+    // Get the address associated to the signer which is connected to  MetaMask
+    const address = await signer.getAddress();
+    // Calls the function to set the ENS or Address
+    await setENSOrAddress(address, web3Provider);
+    return signer;
+  };
+
 }
